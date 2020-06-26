85330cc6ec496624cceac1e4f27bf3e022469d95
glasses-png <cchelangat16@gmail.com>
2020-06-26 11:15:39 +0300

    deploy to heroku

--- awards_project/settings.py
+++ awards_project/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-# import dj_database_url
-# import django_heroku
-# from decouple import config,Csv
+import dj_database_url
+import django_heroku
+from decouple import config,Csv
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -29,7 +29,7 @@ SECRET_KEY = 'qt0_#20n9cts8cf@am3@$x!xn&@zzpdq&ie7(590dq2n@%vr&h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = '*'
+ALLOWED_HOSTS = []
 
 #login redirection
 
@@ -100,9 +100,9 @@ DATABASES = {
     }
 }
 
-# db_env = dj_database_url.config(conn_max_age=500)  
-# DATABASES['default'].update(db_env)  
-# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+db_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_env)  
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -160,4 +160,4 @@ REST_FRAMEWORK = {
     )
 }
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
